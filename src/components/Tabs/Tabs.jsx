3c07882e997b39ff69ad08329a285ddf014b9c0e
Vitalii Martynov <vitalii.martynov@sixt.com>
2025-11-10 01:42:38 -0400

    Considered comments

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -5,7 +5,7 @@ function getContentById(tabs, activeTabId) {
   return (tabs.find(tab => tab.id === activeTabId) || tabs[0]).content;
 }
 
-function getCorrectActiveTabId(tabs, activeTabId) {
+function getResolvedActiveTabId(tabs, activeTabId) {
   return (tabs.find(tab => tab.id === activeTabId) || tabs[0]).id;
 }
 
@@ -17,33 +17,37 @@ const handleTabSelected = (activeTabId, newTabId, onTabSelected) => {
   onTabSelected(newTabId);
 };
 
-export const Tabs = ({ tabs, activeTabId, onTabSelected }) => (
-  <div data-cy="TabsComponent">
-    <div className="tabs is-boxed">
-      <ul>
-        {tabs.map(tab => (
-          <li
-            className={cn({
-              'is-active': tab.id === getCorrectActiveTabId(tabs, activeTabId),
-            })}
-            data-cy="Tab"
-            key={tab.id}
-          >
-            <a
-              href={`#${tab.id}`}
-              data-cy="TabLink"
-              onClick={() =>
-                handleTabSelected(activeTabId, tab.id, onTabSelected)
-              }
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const resolvedActiveTabId = getResolvedActiveTabId(tabs, activeTabId);
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              className={cn({
+                'is-active': tab.id === resolvedActiveTabId,
+              })}
+              data-cy="Tab"
+              key={tab.id}
             >
-              {tab.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <div className="block" data-cy="TabContent">
-        {getContentById(tabs, activeTabId)}
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() =>
+                  handleTabSelected(resolvedActiveTabId, tab.id, onTabSelected)
+                }
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="block" data-cy="TabContent">
+          {getContentById(tabs, activeTabId)}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
