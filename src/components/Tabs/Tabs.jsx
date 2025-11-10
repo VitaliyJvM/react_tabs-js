3de15651114d20d5664721faa6b06a0437c8bd28
Vitalii Martynov <vitalii.martynov@sixt.com>
2025-11-10 01:29:30 -0400

    Initial solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,49 @@
-export const Tabs = () => {};
+import React from 'react';
+import cn from 'classnames';
+
+function getContentById(tabs, activeTabId) {
+  return (tabs.find(tab => tab.id === activeTabId) || tabs[0]).content;
+}
+
+function getCorrectActiveTabId(tabs, activeTabId) {
+  return (tabs.find(tab => tab.id === activeTabId) || tabs[0]).id;
+}
+
+const handleTabSelected = (activeTabId, newTabId, onTabSelected) => {
+  if (newTabId === activeTabId) {
+    return;
+  }
+
+  onTabSelected(newTabId);
+};
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li
+            className={cn({
+              'is-active': tab.id === getCorrectActiveTabId(tabs, activeTabId),
+            })}
+            data-cy="Tab"
+            key={tab.id}
+          >
+            <a
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+              onClick={() =>
+                handleTabSelected(activeTabId, tab.id, onTabSelected)
+              }
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div className="block" data-cy="TabContent">
+        {getContentById(tabs, activeTabId)}
+      </div>
+    </div>
+  </div>
+);
